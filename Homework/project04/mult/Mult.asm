4c2a7b1ce716c26fcc5a7ef8a6b39c4bea3592b5
Carlos Aguilera <18aguilerac@gmail.com>
2023-10-27 23:18:38 -0700

    ease

--- Homework/project04/mult/Mult.asm
+++ Homework/project04/mult/Mult.asm
@@ -28,13 +28,27 @@
 // @END
 // 0;jmp
 
+@R2
+M=0
+
 (LOOP)
 @R0
 D=M
 @END
 D;JEQ
 
+// R2 = R2 + R1
+@R1
+D=M
+@R2
+M=D+M
 
+// R0 = R0 - 1
+@R0
+M=M-1
+
+@LOOP
+0;JMP
 
 // infinite program loop
 (END)
