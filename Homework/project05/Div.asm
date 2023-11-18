b7c2bd7b5c66ce20dc61bbff37068a47c06249cd
Carlos Aguilera <18aguilerac@gmail.com>
2023-11-18 15:42:52 -0800

    update

--- Homework/project05/Div.asm
+++ Homework/project05/Div.asm
@@ -5,24 +5,99 @@ M=0
 @R3
 M=0
 
+@R1
+D=M
+@divisor
+M=D
+
 // Call MAIN function
 @MAIN
 0;JMP
 
-// should not run
 (ALGO_1)
-  @400
-  D=A
+  // quotient = 0
+  @R2
+  M=0;
+
+  // remainder = dividend
   @R0
+  D=M
+  @R3
   M=D
+
+  (DO_WHILE_ALGO_1)
+    // remainder = remainder - divisor
+    @divisor
+    D=M
+    @R3
+    M=M-D
+
+    // quotient += 1
+    @R2
+    M=M+1
+
+    // if (remainder > 0) then JMP to DO_WHILE_ALGO_1
+    @R3
+    D=M
+    @DO_WHILE_ALGO_1
+    D;JGT
+  (END_DO_WHILE_ALGO_1)
+
+  // if (remainder >= 0) then return
+  @R3
+  D=M
+  @ALGO_1_HANDLE_RETURN
+  D;JGE
+
+  // remainder += divisor
+  @divisor
+  D=M
+  @R3
+  M=M+D
+
+  // quotient -= 1
+  @R2
+  M=M-1
+
+  (ALGO_1_HANDLE_RETURN)
+    @ALGO_1_RETURN
+    A=M
+    0;JMP
 (END_ALGO_1)
 
-// should not run
 (ALGO_2)
-  @400
-  D=A
+  // quotient = 0
+  @R2
+  M=0;
+
+  // remainder = dividend
   @R0
+  D=M
+  @R3
   M=D
+
+  (DO_WHILE_ALGO_2)
+    // remainder = remainder + divisor
+    @divisor
+    D=M
+    @R3
+    M=M+D
+
+    // quotient += 1
+    @R2
+    M=M+1
+
+    // if (remainder < 0) then JMP to DO_WHILE_ALGO_2
+    @R3
+    D=M
+    @DO_WHILE_ALGO_2
+    D;JLT
+  (END_DO_WHILE_ALGO_2)
+
+  (ALGO_2_HANDLE_RETURN)
+    @ALGO_2_RETURN
+    A=M
+    0;JMP
 (END_ALGO_2)
 
 (MAIN)
@@ -37,7 +112,7 @@ M=0
   D;JNE
 
   // if (R1 != 0) then JMP to IF_SPECIAL_CASE_2
-  @R1
+  @divisor
   D=M
   @IF_SPECIAL_CASE_2
   D;JNE
@@ -62,7 +137,7 @@ M=0
     D;JEQ
 
     // if (R1 != 0) then JMP to EVALUATE_CASES
-    @R1
+    @divisor
     D=M
     @EVALUATE_CASES
     D;JNE
@@ -91,20 +166,19 @@ M=0
     D;JLT
 
     // if (R1 < 0) then JMP to CASE_2
-    @R1
+    @divisor
     D=M
     @CASE_2
     D;JLT
 
     // at this point R0 and R1 are greater than 0
-    @10
+    // call algo_1
+    @END
     D=A
-    @R2
-    M=D
-    @R3
+    @ALGO_1_RETURN
     M=D
 
-    @END
+    @ALGO_1
     0;JMP
 
     (CASE_2)
@@ -115,18 +189,31 @@ M=0
       D;JLT
 
       // if (R1 > 0) then JMP to CASE_3
-      @R1
+      @divisor
       D=M
       @CASE_3
       D;JGT
 
       // at this point R0 > 0 and R1 < 0
-      @20
+      // negate divisor
+      @divisor
+      M=!M
+      M=M+1
+
+      // call algo_1
+      @CASE_2_NEXT
       D=A
-      @R2
+      @ALGO_1_RETURN
       M=D
-      @R3
-      M=D
+
+      @ALGO_1
+      0;JMP
+
+      (CASE_2_NEXT)
+        // negate quotient
+        @R2
+        M=!M
+        M=M+1
 
       @END
       0;JMP
@@ -140,18 +227,26 @@ M=0
       D;JGT
 
       // if (R1 < 0) then JMP to CASE_4
-      @R1
+      @divisor
       D=M
       @CASE_4
       D;JLT
 
       // at this point R0 < 0 and R1 > 0
-      @30
+      // call algo_2
+      @CASE_3_NEXT
       D=A
-      @R2
+      @ALGO_2_RETURN
       M=D
-      @R3
-      M=D
+
+      @ALGO_2
+      0;JMP
+
+      (CASE_3_NEXT)
+        // negate quotient
+        @R2
+        M=!M
+        M=M+1
 
       @END
       0;JMP
@@ -159,14 +254,18 @@ M=0
 
     (CASE_4)
       // at this point R0 < 0 and R1 < 0
-      @40
+      // negate divisor
+      @divisor
+      M=!M
+      M=M+1
+
+      // call algo_2
+      @END
       D=A
-      @R2
-      M=D
-      @R3
+      @ALGO_2_RETURN
       M=D
 
-      @END
+      @ALGO_2
       0;JMP
     (END_CASE_4)
 
@@ -177,4 +276,3 @@ M=0
 (END)
   @END
   0;JMP
-
